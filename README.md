391369e796414f801870f27bb3fdda84790bb7b6
BeSaRa <ahmedbesara@gmailcom>
2025-09-27 01:25:46 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularEgypt
+# TodoTest
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.3.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.3.
 
 ## Development server
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,12 @@
-import {
-  ApplicationConfig,
-  inject,
-  provideAppInitializer,
-  provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { LookupService } from '../services/lookup-service';
-import { catchError, forkJoin, of } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    // provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
-    provideAppInitializer(() => {
-      const lookupService = inject(LookupService)
-      return forkJoin([lookupService.load()]).pipe(catchError(() => of()))
-    })
+    provideRouter(routes)
   ]
 };
